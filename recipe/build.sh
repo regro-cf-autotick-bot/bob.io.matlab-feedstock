81a0bc0c857a3420fca22141a9a7988323aa9abd
Amir Mohammadi <183.amir@gmail.com>
2016-04-27 14:28:39 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,7 @@
 
 if [[ `uname` == 'Darwin' ]]; then
     export MACOSX_DEPLOYMENT_TARGET=10.9
+    export CFLAGS="${CFLAGS} --pthread"
 fi
 
 $PYTHON -B setup.py install --single-version-externally-managed --record record.txt
